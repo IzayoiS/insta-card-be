1a10b3ed7c6f8fea9e104f084e5426662966f19f
IzayoiS <iqbalmhasby54@gmail.com>
2025-07-10 22:20:52 +0700

    feat: add cors url

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,11 @@ async function init() {
 
     const app = express();
 
-    app.use(cors());
+    app.use(
+      cors({
+        origin: 'https://insta-card-fe.vercel.app',
+      }),
+    );
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
